6aa3f349a8238d49c40aa84e941f73ad7c095b8e
muhammadyunusuz <yunuzmuhammad1@gmail.com>
2021-11-01 05:34:04 +0500

    fix some issues

--- modules/mongo.js
+++ modules/mongo.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 // const CategorySchema = require("../models/CategoriesModel");
 
 async function mongo() {
-	await mongoose.connect("mongodb://localhost:27017/test_uchun_mongoose");
+	await mongoose.connect(
+		"mongodb+srv://muhammad:<password>@cluster0.kxp1v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+	);
 }
 
 module.exports = mongo;
